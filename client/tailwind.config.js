e7bb1362fc50355a4b82efa4612a815b9256435c
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-09-04 18:24:57 +0530

    add theme colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        c_green: "#03b57b",
+        c_white: "#fefefe",
+        c_grey: "595456",
+        c_black: "#323232",
+        c_lightgrey: "#ea940c"
+      }
+    },
   },
   plugins: [],
 };
