3ada60338bdd60e9488cf830f7ce49896c35dd4f
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-09-04 20:03:40 +0530

    integrate react-hook-form in auth component

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,54 +1,84 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const Auth = () => {
   const [isSignup, setIsSignup] = useState(false);
+  const { register, handleSubmit, control } = useForm();
 
   const switchMode = () => {
     setIsSignup((prevIsSignup) => !prevIsSignup);
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col">
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
         <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-          <h1 className="mb-8 text-3xl text-center">Sign in</h1>
-          {isSignup && (
+          <h1 className="mb-8 text-3xl text-center">
+            {isSignup ? "Sign Up" : "Sign in"}
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {isSignup && (
+              <input
+                type="text"
+                className="block border border-grey-light w-full p-3 rounded mb-4"
+                placeholder="Full Name"
+                {...register("name", {
+                  required: {
+                    value: true,
+                    message: "Please enter your name",
+                  },
+                })}
+              />
+            )}
             <input
               type="text"
               className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="fullname"
-              placeholder="Full Name"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Please enter your email",
+                },
+              })}
+              placeholder="Email"
             />
-          )}
-          <input
-            type="text"
-            className="block border border-grey-light w-full p-3 rounded mb-4"
-            name="email"
-            placeholder="Email"
-          />
 
-          <input
-            type="password"
-            className="block border border-grey-light w-full p-3 rounded mb-4"
-            name="password"
-            placeholder="Password"
-          />
-          {isSignup && (
             <input
               type="password"
               className="block border border-grey-light w-full p-3 rounded mb-4"
-              name="confirm_password"
-              placeholder="Confirm Password"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Please enter your password",
+                },
+              })}
+              placeholder="Password"
             />
-          )}
-          <button
-            type="submit"
-            className="bg-custom_green w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-          >
-            Create Account
-          </button>
+            {isSignup && (
+              <input
+                type="password"
+                className="block border border-grey-light w-full p-3 rounded mb-4"
+                {...register("confirmPassword", {
+                  required: {
+                    value: true,
+                    message: "Please confirm your password",
+                  },
+                })}
+                placeholder="Confirm Password"
+              />
+            )}
+            <button
+              type="submit"
+              className="bg-custom_green w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+            >
+              {isSignup ? "Create Account" : "Sign in"}
+            </button>
+          </form>
         </div>
-
         <div className="text-grey-dark mt-6">
           {isSignup ? "Already have an account?" : "Don't have an account?"}
           <button
@@ -59,6 +89,7 @@ const Auth = () => {
           </button>
         </div>
       </div>
+      <DevTool control={control} />
     </div>
   );
 };
