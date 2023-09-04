b33cd09863694c89b4c7caeac1aeba84a2cb47d7
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-09-04 19:15:24 +0530

    create auth form ui

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,5 +1,66 @@
+import { useState } from "react";
+
 const Auth = () => {
-  return <div>Auth</div>;
+  const [isSignup, setIsSignup] = useState(false);
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+  };
+
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+          <h1 className="mb-8 text-3xl text-center">Sign in</h1>
+          {isSignup && (
+            <input
+              type="text"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="fullname"
+              placeholder="Full Name"
+            />
+          )}
+          <input
+            type="text"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="email"
+            placeholder="Email"
+          />
+
+          <input
+            type="password"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="password"
+            placeholder="Password"
+          />
+          {isSignup && (
+            <input
+              type="password"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="confirm_password"
+              placeholder="Confirm Password"
+            />
+          )}
+          <button
+            type="submit"
+            className="bg-custom_green w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+          >
+            Create Account
+          </button>
+        </div>
+
+        <div className="text-grey-dark mt-6">
+          {isSignup ? "Already have an account?" : "Don't have an account?"}
+          <button
+            onClick={switchMode}
+            className="no-underline border-b border-blue text-blue ml-2 text-custom_orange"
+          >
+            {isSignup ? "Sign in" : "Sign Up"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
