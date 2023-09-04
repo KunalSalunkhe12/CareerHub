255d1a408ec284e93154dcf55b19d687f8e8405a
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-09-04 18:59:31 +0530

    add link component to navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <nav className="px-12 py-3 flex justify-between items-center bg-custom_black">
-      <h1 className="text-3xl font-bold cursor-pointer text-custom_green">
-        CareerHub
-      </h1>
+      <Link to="/">
+        <h1 className="text-3xl font-bold cursor-pointer text-custom_green">
+          CareerHub
+        </h1>
+      </Link>
       <ul className="flex items-center gap-7 ml-6 text-white">
         <li className="font-semibold hover:text-custom_orange cursor-pointer">
           Ai Resume Builder
@@ -18,11 +22,12 @@ const Header = () => {
           Career Blogs
         </li>
       </ul>
-      <ul className="flex gap-3 mr-10">
-        <li className="px-5 py-1 rounded-lg font-bold bg-custom_green text-white hover:bg-custom_black border-2 border-custom_green cursor-pointer">
-          Sign in
-        </li>
-      </ul>
+      <Link
+        to="/auth"
+        className="px-5 py-1 rounded-lg font-bold bg-custom_green text-white hover:bg-custom_black border-2 border-custom_green cursor-pointer"
+      >
+        Sign in
+      </Link>
     </nav>
   );
 };
