c2d1de44eb16b8d87caab6480d896e851d233b80
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-09-07 16:24:16 +0530

    create user schema model

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: function (value) {
+                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+            },
+            message: "Invalid email address format",
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, "Password length must be at least 6 characters"],
+    },
+    id: {
+        type: String,
+        unique: true,
+    },
+});
+
+export default mongoose.model("User", userSchema);
