207042d0f6ac6e04df3ad523e2b53e6a87d4267a
Saloni Pandey <salonipandey014@gmail.com>
2024-02-29 13:03:11 +0530

    created like feat

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,13 @@
 import express from "express";
-
-import { getPosts, createPost, deletePost } from "../controllers/post.js";
-// import {upload} from '../middlewares/multer.js';
+import { getPosts, createPost, deletePost, likePost } from "../controllers/post.js";
 import { upload } from "../middlewares/multer.js";
-
+import auth from "../middlewares/auth.js";
 const router = express.Router();
 
 router.get("/", getPosts);
-router.post("/", upload.single("file"), createPost);
-router.delete("/:id", deletePost);
+router.post("/", upload.single("file"), auth, createPost);
+router.delete("/:id", auth, deletePost);
+
+router.patch("/:id/like", auth, likePost );
 
 export default router;
